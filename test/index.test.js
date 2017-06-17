9cf0fdc9a8aafc5ce8e6df27a723bb2734ce0eaf
Philip Knape <philip.knape@gmail.com>
2017-06-18 01:03:21 +0200

    migrate to jest instead of mocha

--- test/index.test.js
+++ test/index.test.js
@@ -1,74 +1,74 @@
+/* globals expect */
 import datum from '../src/index';
-import { expect } from 'chai';
 
 describe('Util functions', () => {
   describe('datum to decimal', () => {
-    it('should convert a number to correct text', () => {
+    test('should convert a number to correct text', () => {
       expect(datum(new Date()))
-      .to.equal('0 minutes ago');
+      .toBe('0 minutes ago');
     });
 
-    it('should convert a date 1 minute ago to correct text', () => {
+    test('should convert a date 1 minute ago to correct text', () => {
       const d = new Date();
       const newDate = d.setMinutes(d.getMinutes() - 1);
       expect(datum(newDate))
-      .to.equal('1 minute ago');
+      .toBe('1 minute ago');
     });
 
-    it('should convert a date 20 minutes ago to correct text', () => {
+    test('should convert a date 20 minutes ago to correct text', () => {
       const d = new Date();
       const newDate = d.setMinutes(d.getMinutes() - 20);
       expect(datum(newDate))
-      .to.equal('20 minutes ago');
+      .toBe('20 minutes ago');
     });
 
-    it('should convert a date 5 days ago to correct text', () => {
+    test('should convert a date 5 days ago to correct text', () => {
       const d = new Date();
       const newDate = d.setDate(d.getDate() - 5);
       expect(datum(newDate))
-      .to.equal('5 days ago');
+      .toBe('5 days ago');
     });
 
-    it('should convert a date 8 days ago to correct text', () => {
+    test('should convert a date 8 days ago to correct text', () => {
       const d = new Date();
       const newDate = d.setDate(d.getDate() - 8);
       expect(datum(newDate))
-      .to.equal('8 days ago');
+      .toBe('8 days ago');
     });
 
-    it('should convert a date 9 days ago to correct text', () => {
+    test('should convert a date 9 days ago to correct text', () => {
       const d = new Date();
       const newDate = d.setDate(d.getDate() - 9);
       expect(datum(newDate))
-      .to.equal('9 days ago');
+      .toBe('9 days ago');
     });
 
-    it('should convert a date 40 days ago to correct text', () => {
+    test('should convert a date 40 days ago to correct text', () => {
       const d = new Date();
       const newDate = d.setDate(d.getDate() - 40);
       expect(datum(newDate))
-      .to.equal('1 month ago');
+      .toBe('1 month ago');
     });
 
-    it('should convert a date 80 days ago to correct text', () => {
+    test('should convert a date 80 days ago to correct text', () => {
       const d = new Date();
       const newDate = d.setDate(d.getDate() - 80);
       expect(datum(newDate))
-      .to.equal('2 months ago');
+      .toBe('2 months ago');
     });
 
-    it('should convert a date 365 days ago to correct text', () => {
+    test('should convert a date 365 days ago to correct text', () => {
       const d = new Date();
       const newDate = d.setDate(d.getDate() - 365);
       expect(datum(newDate))
-      .to.equal('1 year ago');
+      .toBe('1 year ago');
     });
 
-    it('should show new exception text', () => {
+    test('should show new exception text', () => {
       expect(datum(new Date(), {
         exception: 'Just now'
       }))
-      .to.equal('Just now');
+      .toBe('Just now');
     });
   });
 });
